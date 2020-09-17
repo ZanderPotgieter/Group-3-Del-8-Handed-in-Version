ff3640e459db88d2a025d134f35d0a07b8635196
Ashley-Gwangwadza <ashleygwa@gmail>
2020-09-17 18:54:18 +0200

    Update login.service.ts

--- G3-Maverick-Solutions/Angular/src/app/login.service.ts
+++ G3-Maverick-Solutions/Angular/src/app/login.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpClientModule} from "@angular/common/http"; 
+import { Observable } from 'rxjs'; 
+import { User} from "./user";
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+ constructor(private http: HttpClient) { }
+
+  url = 'https://localhost:44399/Api/Login'
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  registerUser(user : User){
+    return this.http.post(this.url + '/registerUser', user, this.httpOptions)
+  }
+
+ loginUser(user : User){
+    return this.http.post(this.url + '/loginUser', user, this.httpOptions) 
+  }
+
+
+  getUserDetails(session: any){
+    return this.http.post(this.url + '/getUserDetails',session, this.httpOptions)
+  }
 }

85538a86cc081d395d2044db5be28488e42708cc
ZANDER POTGIETER <u18214496@tuks.co.za>
2020-09-18 13:58:54 +0200

    login front end changes

--- G3-Maverick-Solutions/Angular/src/app/app.component.ts
+++ G3-Maverick-Solutions/Angular/src/app/app.component.ts
@@ -15,12 +15,39 @@ export class AppComponent {
   title = 'ORDRA';
   dateVal = new Date();
 
-  // constructor(private router: Router) { }
+  constructor(private router: Router) { }
 
-  // logout()
-  // {
-  //   localStorage.removeItem("accessToken");
-  //   this.router.navigate(["login"]);
-  // }
+showLogin: boolean = true;
+showNav: boolean = false;
+showRegister: boolean = false;
+
+login(){
+  this.showLogin = false;
+  this.showNav = true;
+  this.showRegister = false;
+}
+
+home(){
+  this.showLogin = true;
+  this.showNav = false;
+  this.showRegister = false;
+}
+
+register(){
+  this.showLogin = false;
+  this.showNav = false;
+  this.showRegister = true;
+}
+
+Validate() {
+        var password = document.getElementById("txtPassword")
+        var confirmPassword = document.getElementById("txtConfirmPassword");
+        if (password != confirmPassword) {
+            alert("Passwords do not match.");
+            return false;
+        }
+        return true;
+    }
+
 
 }

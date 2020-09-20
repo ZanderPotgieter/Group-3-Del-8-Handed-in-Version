e04d87a0d957db5e34c92a33edf20359125907a0
ZANDER POTGIETER <u18214496@tuks.co.za>
2020-09-20 23:30:57 +0200

    validation

--- G3-Maverick-Solutions/Angular/src/app/add-customer/add-customer.component.ts
+++ G3-Maverick-Solutions/Angular/src/app/add-customer/add-customer.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import {Customer} from '../customer-management/customer';
 import { NgModule } from '@angular/core';
 import {CustomerService} from '../customer-management/customer.service';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-customer',
@@ -11,7 +14,8 @@ import {CustomerService} from '../customer-management/customer.service';
 })
 export class AddCustomerComponent implements OnInit {
 
-  constructor(private api: CustomerService, private router: Router) { }
+  constructor(private api: CustomerService, private router: Router, private fb: FormBuilder) { }
+  cusForm: FormGroup;
    customer : Customer = new Customer();
    responseMessage: string = "Request Not Submitted";
   
@@ -19,6 +23,17 @@ export class AddCustomerComponent implements OnInit {
   
 
   ngOnInit(): void {
+
+    this.cusForm= this.fb.group({  
+      CusName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25), Validators.pattern('[a-zA-Z ]*')]],  
+      CusSurname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25), Validators.pattern('[a-zA-Z ]*')]],   
+      CusCell: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[0-9]*')]],   
+      CusEmail: ['', [Validators.required, Validators.email]], 
+      CusStreetNr: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(8), Validators.pattern('[0-9 ]*')]],
+      CusStreet: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25), Validators.pattern('[a-zA-Z ]*')]],
+      CusCode: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(4), Validators.pattern('[0-9]*')]], 
+      CusSuburb: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25), Validators.pattern('[a-zA-Z ]*')]],       
+    }); 
   }
 
   addCustomer(){

ebfb59348c2fbbfecae7fbda65b32314d456b3c4
JGMon <jessica.gina.mon@gmail.com>
2020-09-20 21:37:37 +0200

    Product Category Completed
    Validation for CRUD Functional.
    Updated the Search Product Category.
    Can search for all product categories or a  specific product category.

--- G3-Maverick-Solutions/Angular/src/app/product-management/product-category/add-product-category/add-product-category.component.ts
+++ G3-Maverick-Solutions/Angular/src/app/product-management/product-category/add-product-category/add-product-category.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; 
 import { ProductCategoryService } from '../../product-category.service';
 import { ProductCategory } from '../../product-category';
-
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product-category',
@@ -11,12 +11,18 @@ import { ProductCategory } from '../../product-category';
 })
 export class AddProductCategoryComponent implements OnInit {
 
-  constructor(private productCategoryService: ProductCategoryService, private router: Router) { }
-
+  constructor(private productCategoryService: ProductCategoryService, private router: Router,  private fb: FormBuilder) { }
+  pcatForm: FormGroup;
   productCategory : ProductCategory = new ProductCategory();
   responseMessage: string = "Request Not Submitted";
 
-  ngOnInit(): void{
+  ngOnInit(){
+    this.pcatForm= this.fb.group({  
+      PCatName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25), Validators.pattern('[a-zA-Z ]*')]],  
+      PCatDescription: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]*')]],  
+      
+       
+    });  
   }
 
   Save() {
